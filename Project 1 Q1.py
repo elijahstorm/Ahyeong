dc27e573c504aa603b8cd4f525208761f243d134
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-11-14 14:07:50 +0900

    adding comments

--- Project 1 Q1.py
+++ Project 1 Q1.py
@@ -5,37 +5,48 @@ def menu():
     print("3: To multiply fraction")
     print("4: To divide fraction")
     print("9: To exit the program")
-    choice = int(input())
-    return choice
+    return getNumber("Enter a number: ")
+
+# since all inputs are numbers, we can make just one getNumber function
+def getNumber(prompt: str):
+    while True:
+        try:
+            choice = int(input(prompt))
+            return choice
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 def addFractions(num1:int,den1:int,num2:int,den2:int):
     print(f"({num1}/{den1}) + ({num2}/{den2}) = {(num1*den2 + num2*den1)}/{den1*den2}")
 
 def subtractFractions(num1: int, den1: int, num2: int, den2: int):
-    print(f"{num1}/{den1} - {num2}/{den2} = {(num1 * den2 - num2 * den1)}/{den1 * den2}")
+    print(f"({num1}/{den1}) - ({num2}/{den2}) = {(num1 * den2 - num2 * den1)}/{den1 * den2}")
 
 def multiplyFractions(num1: int, den1: int, num2: int, den2: int):
-    print(f"{num1}/{den1} * {num2}/{den2} = {(num1 * num2)}/{den1 * den2}")
+    print(f"({num1}/{den1}) * ({num2}/{den2}) = {(num1 * num2)}/{den1 * den2}")
 
 def divideFractions(num1: int, den1: int, num2: int, den2: int):
-    print(f"{num1}/{den1} / {num2}/{den2} = {(num1 * den2)}/{den1 * num2}")
+    print(f"({num1}/{den1}) / ({num2}/{den2}) = {(num1 * den2)}/{den1 * num2}")
 
+# all inputs will be the same, so we can have just one imput function
 def getInputs(action):
     print("For fraction 1")
-    num1 = int(input("Enter the numerator: "))
-    den1 = int(input("Enter the denominator: "))
+    num1 = getNumber("Enter the numerator: ")
+    den1 = getNumber("Enter the denominator: ")
     while den1 == 0:
         print("The denominator must be nonzero.")
-        den1 = int(input("Enter the denominator: "))
+        den1 = getNumber("Enter the denominator: ")
     print("For fraction 2")
-    num2 = int(input("Enter the numerator: "))
-    den2 = int(input("Enter the denominator: "))
+    num2 = getNumber("Enter the numerator: ")
+    den2 = getNumber("Enter the denominator: ")
     while den2 == 0:
         print("To divide, the second fraction must be nonzero. ")
-        den2 = int(input("Enter a nonzero number for the numerator: "))
+        den2 = getNumber("Enter a nonzero number for the numerator: ")
     action(num1, den1, num2, den2)
 
 def main():
+    # since choice is not one of the options, this works to skip directly to the menu
+    # which is located at the end of the while loop
     choice = 0
     while choice != 9:
         if choice == 1:
